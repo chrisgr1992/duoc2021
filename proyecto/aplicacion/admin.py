e3976ec11b52d147ce3892ff9f52f9a5f652c5b7
chrisgr1992 <82065844+chrisgr1992@users.noreply.github.com>
2021-06-17 18:32:29 -0400

    Delete proyecto/aplicacion directory

--- proyecto/aplicacion/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Vehiculos
-# Register your models here.
-
-admin.site.register(Vehiculos)
--- proyecto/aplicacion/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Vehiculos
-
-class VehiculosFormulario (forms.ModelForm):
-    class Meta:
-        model = Vehiculos
-        fields = ('autor','vin','patente','año_fabricacion','fecha_recepcion','marca','modelo',)
